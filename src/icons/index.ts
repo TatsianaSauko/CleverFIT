b606bae1487f6ae6391e3de32c8b90427cf97cea
TatsianaSauko <miljanzowa@mail.ru>
2024-02-10 16:21:51 +0300

    feat: create new icons with color change

---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+export { HeartIcon } from './HeartIcon';
+export { TrophyIcon } from './TrophyIcon';
+export { CalendarIcon } from './CalendarIcon';
+export { IdCardIcon } from './IdCardIcon';
+export { SettingsIcon } from './SettingsIcon';
